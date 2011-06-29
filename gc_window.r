40ea76329e8dc54a1383b7d49d57c31829ea1cec
Ntino Krampis <agbiotec@yahoo.com>
2011-06-29 14:34:42 -0400

    updated dual barplot with colors, still need range and x-axis numbers

--- gc_window.r
+++ gc_window.r
@@ -44,11 +44,73 @@ slidingwindowfreq <- function(windowsize, inputdir, binsize)
    } 
 
    bins <- seq(0,1,by=binsize)
-   hist1<-hist(GCs,breaks=bins,plot=FALSE, xlab='GC content %', ylab='% of 1kb bins with this GC content' )
+   hist1<-hist(GCs,breaks=bins,plot=FALSE)
+   print(hist1$counts)
    hist1$counts <- hist1$counts*100/sum(hist1$counts)
+   print(hist1$counts)
    plot(hist1)
 }
 
+
+slidingwindowfreqtwosets <- function(windowsize, inputdir1, inputdir2, binsize)
+{
+   GCs1 <- numeric()
+   v <- 0 
+   files <- list.files(inputdir1,full.names=TRUE)
+
+   for (f in files) {
+       singleseqvec <- read.fasta(file = f)
+       singleseqnuc<-singleseqvec[[1]]
+       lengthseq = length(singleseqnuc) 
+
+       if (lengthseq > windowsize) {
+
+            starts <- seq(1, length(singleseqnuc)-windowsize, by = windowsize)
+            n <- length(starts)    
+     
+            for (i in 1:n) {
+                 chunk <- singleseqnuc[starts[i]:(starts[i]+windowsize-1)]
+                 GC <- GC(chunk)
+                 GCs1[v] <- GC
+                 v <- v+1
+            }
+       }
+   } 
+
+
+   GCs2 <- numeric()
+   v <- 0 
+   files <- list.files(inputdir2,full.names=TRUE)
+
+   for (f in files) {
+       singleseqvec <- read.fasta(file = f)
+       singleseqnuc<-singleseqvec[[1]]
+       lengthseq = length(singleseqnuc) 
+
+       if (lengthseq > windowsize) {
+
+            starts <- seq(1, length(singleseqnuc)-windowsize, by = windowsize)
+            n <- length(starts)   
+     
+            for (i in 1:n) {
+                 chunk <- singleseqnuc[starts[i]:(starts[i]+windowsize-1)]
+                 GC <- GC(chunk)
+                 GCs2[v] <- GC
+                 v <- v+1
+            }
+       }
+   } 
+
+   bins <- seq(0,1,by=binsize)
+   hist1<-hist(GCs1,breaks=bins,plot=FALSE) 
+   hist2<-hist(GCs2,breaks=bins,plot=FALSE) 
+   hist1$counts <- hist1$counts*100/sum(hist1$counts)
+   hist2$counts <- hist2$counts*100/sum(hist2$counts)
+   
+   data12 <- t(cbind(hist1$counts,hist2$counts))
+   barplot(data12,beside=TRUE, space=rep(0,2*ncol(data12)), col=c("red", "green"), main='red bars : S.downei,  green bars: unmapped', xlab='GC content ratio', ylab='% of 1kb bins with this GC content' )
+   axis(1, at=bins)
+}
 
 
 gcvssize <- function(inputdir)
